2ea44f3258633ee8bad7b5b402f14936dbfde2c4
binarycasper <34405930+binarycasper@users.noreply.github.com>
2018-12-03 16:58:51 -0300

    Add basic HTML and JS algorithm

---
+++ index.js
@@ -0,0 +1,42 @@
+function calculate()
+{
+    var numberInput = document.getElementById("number").value;
+    var output = ''; 
+    
+    var primes = [];
+    var sqrtnumberInput = Math.sqrt(numberInput) - 2;
+    var numbers = new Array();
+
+    for (var i = 2; i <= numberInput; i++) 
+        numbers.push(i); 
+    
+    for (var i = 0; i <= sqrtnumberInput; i++) {
+    var p = numbers[i]
+    if (p)
+        for (var j = p * p - 2; j < numbers.length; j += p)
+            numbers[j] = 0;
+    }
+    
+    for (var i = 0; i < numbers.length; i++) {
+        var p = numbers[i];
+        if (p)
+            primes.push(p);
+    }
+
+    for (var i = 0; i < numbers.length; i++)
+    {
+        if (numbers[i] != 0)
+        output = output + '<div class="gridItem" id="prime">' + (i + 2) + '</div>'; 
+        else  
+        output = output + '<div class="gridItem">' + (i + 2) + '</div>';
+    }
+
+    document.getElementById("gridContainer").innerHTML = output; 
+}
+
+
+
+
+
+
+
