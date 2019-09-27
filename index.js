08a2192f986c49f35a81d4fd45b8115a7663325b
binarycasper <binarycasper@gmail.com>
2019-09-27 17:50:04 -0300

    make algorithm more efficient

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
+var index;
+var numberInput;
+var numbers; 
+var primes;
+
 function calculate()
 {
-    var numberInput = document.getElementById("number").value;
+    numberInput = document.getElementById("number").value;
+    index = 0;
+    numbers = new Array();
+    primes = new Array();
     var output = ''; 
-    
-    var primes = [];
-    var sqrtnumberInput = Math.sqrt(numberInput) - 2;
-    var numbers = new Array();
 
-    for (var i = 2; i <= numberInput; i++) 
+    for (var i = 2; i <= numberInput; i++) {
         numbers.push(i); 
-    
-    for (var i = 0; i <= sqrtnumberInput; i++) {
-    var p = numbers[i]
-    if (p)
-        for (var j = p * p - 2; j < numbers.length; j += p)
-            numbers[j] = 0;
-    }
-    
-    for (var i = 0; i < numbers.length; i++) {
-        var p = numbers[i];
-        if (p)
-            primes.push(p);
     }
 
-    for (var i = 0; i < numbers.length; i++)
-    {
-        if (numbers[i] != 0)
-        output = output + '<div id="prime" class="numberContainer">' + (i + 2) + '</div>'; 
-        else  
-        output = output + '<div id="notprime" class="numberContainer">' + (i + 2) + '</div>';
+    while (index < numbers.length) {
+        doChunk();
+        index++;
+    } 
+
+    for (var i = 0; i < numbers.length; i++) {
+        if (numbers[i] != 0) {
+            primes.push(numbers[i]);
+        }
+    }
+
+    for (var i = 0; i < primes.length; i++) {
+        output = output + '<div class="numberContainer">' + (primes[i]) + '</div>'; 
     }
 
     document.getElementById("gridContainer").innerHTML = output; 
 }
 
+function doChunk() {
+    var chunk = 10;
+    var count = chunk;
+    var sqrtnumberInput = Math.sqrt(numberInput) - 2;
+
+    while (count-- && index < numbers.length) {
+        for (var i = 0; i <= sqrtnumberInput; i++) {
+            var p = numbers[i]
+            if (p) {
+                for (var j = p * p - 2; j < numbers.length; j += p) {
+                    numbers[j] = 0;
+                }
+            }
+        }
+    }
+} 
 
 
 
